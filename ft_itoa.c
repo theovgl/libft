125fa490cb0d33b5234dd23653006c2a7d49bbd3
tvogel <tvogel@student.42.fr>
2020-11-30 12:12:15 +0100

    Fix : Plus de leak sur itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,52 +6,50 @@
 /*   By: tvogel <tvogel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/20 14:50:05 by tvogel            #+#    #+#             */
-/*   Updated: 2020/11/28 16:45:53 by tvogel           ###   ########.fr       */
+/*   Updated: 2020/11/30 12:05:46 by tvogel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int		len(int n)
+int			len(unsigned int n)
 {
-	int length;
+	unsigned int	i;
 
-	length = 0;
-	if (n < 0)
-	{
-		n = n * -1;
-		length++;
-	}
-	while (n > 0)
-	{
-		n = n / 10;
-		length++;
-	}
-	return (length);
-}
-
-char	*ft_itoa(int n)
-{
-	char	*dest;
-	int		nb;
-	int		i;
-	int		length;
-
-	nb = (n < 0 ? -n : n);
-	length = len(n);
 	i = 0;
-	if (!(dest = malloc(sizeof(char) * length + 1 + (n < 0 ? 1 : 0))))
-		return (NULL);
-	if (n < 0)
-		dest[i] = '-';
-	i = length;
 	while (n >= 10)
 	{
-		dest[i] = nb % 10 + '0';
+		n /= 10;
+		i++;
+	}
+	return (i + 1);
+}
+
+char				*ft_itoa(int n)
+{
+	char			*dest;
+	unsigned int	length;
+	unsigned int	nb;
+	unsigned int	i;
+
+	nb = (n < 0 ? -n : n);
+	length = len(nb);
+	i = 0;
+	if (!(dest = (char *)malloc(sizeof(char) * length + 1 + (n < 0 ? 1 : 0))))
+		return (NULL);
+	if (n < 0)
+	{
+		dest[i] = '-';
+		length++;
+	}
+	i = length - 1;
+	while (nb >= 10)
+	{
+		dest[i] = nb % 10 + 48;
 		nb /= 10;
 		i--;
 	}
-	dest[i] = nb % 10 + '0';
+	dest[i] = nb % 10 + 48;
 	dest[length] = '\0';
 	return (dest);
 }
